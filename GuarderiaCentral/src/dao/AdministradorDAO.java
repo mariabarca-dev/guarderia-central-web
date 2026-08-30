d7be9c090dfa5061a930f0e4e0e48aafe8effa93
hector <hector.machaca@alu.inspt.utn.edu.ar>
2026-08-30 16:56:44 -0300

    prueva hector

--- GuarderiaCentral/src/dao/AdministradorDAO.java
+++ GuarderiaCentral/src/dao/AdministradorDAO.java
@@ -2,7 +2,7 @@ package dao;
 
 import java.util.List;
 import model.Administrador;
-
+//prueva conecion hector
 public interface AdministradorDAO extends GenericDAO<Administrador, Integer> {
 
     boolean existeAdministrador(String username);
